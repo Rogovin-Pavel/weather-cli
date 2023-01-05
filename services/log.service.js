b0756f9bbe8f2bca41061bbae4060e51b3a09bf7
p.rogovin <p.rogovin@devinotele.com>
2023-01-05 16:20:27 +0400

    [V36] arg helper and log service are added

---
+++ services/log.service.js
@@ -0,0 +1,16 @@
+import chalk from 'chalk';
+
+const help = () =>
+  console.log(
+    `
+      ${chalk.bgCyan('Help')}
+      Без параметров - вывод погоды
+      ${chalk.bgGreen('-h')} - для вывода помощи
+      ${chalk.bgYellow('-s')} [CITY] - для установки города
+      ${chalk.bgRed('-t')} [API_KEY] - для сохранения токена
+    `
+  );
+const alert = (error) => console.log(chalk.bgRed(`Error: ${error}`));
+const success = (success) => console.log(chalk.bgGreen(`Success: ${success}`));
+
+export { help, alert, success };
